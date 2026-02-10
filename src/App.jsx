ff29e3d21696f97db5fae30a33a1c46cfe389b4f
TizianoN <hello@tizianonicolai.com>
2026-02-10 11:56:45 +0100

    reset css & html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
 export default function App() {
-  return <h1>Demo Vite & React</h1>;
+  return (
+    <div className="py-4 container">
+      <div className="text-center">
+        <h1>Demo Vite & React</h1>
+        <p className="text-muted">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+          magni numquam nemo, iure sint quibusdam suscipit commodi rem adipisci
+          harum. Quo quidem molestias magni molestiae ullam eius rem corporis
+          obcaecati?
+        </p>
+      </div>
+    </div>
+  );
 }
